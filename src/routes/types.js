bf4af55b1ac9be0c3e5c9d2da11546ba01ec323f
etchepared <etcheparede@gmail.com>
2022-08-15 23:01:24 -0300

    heroku config

--- src/routes/types.js
+++ src/routes/types.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const router = Router();
 // const { Sequalize, Op } = require("sequelize");
 
-router.get("/", async (req, res, next) => {
+router.get("/types", async (req, res, next) => {
   try {
     const types = await axios
       .get("https://pokeapi.co/api/v2/type")
